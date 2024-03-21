625784329fdc602927481e58eab4e90466bd7abd
shjang1025 <shjang1025@gmail.com>
2024-03-21 11:18:49 -0700

    setup project

---
+++ pokedex-rails-backend/db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_181136) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "pokemons", force: :cascade do |t|
+    t.integer "number", null: false
+    t.string "name", null: false
+    t.integer "attack", null: false
+    t.integer "defense", null: false
+    t.string "poke_type", null: false
+    t.string "image_url", null: false
+    t.boolean "captured", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_pokemons_on_name", unique: true
+    t.index ["number"], name: "index_pokemons_on_number", unique: true
+  end
+
+end
